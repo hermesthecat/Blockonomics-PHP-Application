0e7d87ce5051405ea3fc4f387afdd6f9bff804cc
abdullah azad <4947845+hermesthecat@users.noreply.github.com>
2023-09-05 16:32:12 +0300

    Update index.php

--- app/index.php
+++ app/index.php
@@ -4,15 +4,18 @@ include_once "functions.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bitcoin store</title>
-    <!-- Bootstrap -->
+
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-    <!-- Custom CSS -->
+
     <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
 
     <!-- Navigation bar -->
@@ -25,14 +28,14 @@ include_once "functions.php";
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="index.php">Store <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item active">
-                    <a class="nav-link" href="orders.php">Purchases <span class="sr-only">(current)</span></a>
-                </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="index.php">Store <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="invoices.php">Invoices</a>
+                    </li>
                 </ul>
-                
+
             </div>
         </div>
     </nav>
@@ -45,8 +48,8 @@ include_once "functions.php";
                 // Get and display all products
                 $sql = "SELECT * FROM `products` ORDER BY `id` DESC";
                 $result = mysqli_query($conn, $sql);
-                while($row = mysqli_fetch_assoc($result)){
-                    ?>
+                while ($row = mysqli_fetch_assoc($result)) {
+                ?>
                     <div class="product">
                         <div class="card" style="width: 95%;margin:0 auto;">
                             <div class="card-body">
@@ -57,7 +60,7 @@ include_once "functions.php";
                             </div>
                         </div>
                     </div>
-                    <?php
+                <?php
                 }
                 ?>
             </div>
@@ -69,4 +72,5 @@ include_once "functions.php";
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 </body>
+
 </html>
