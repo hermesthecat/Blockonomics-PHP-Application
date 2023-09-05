943e965ddb1abc726a99927422cbc11bb0c5b8c6
abdullah azad <4947845+hermesthecat@users.noreply.github.com>
2023-09-05 16:30:46 +0300

    Update index.php

--- app/check/index.php
+++ app/check/index.php
@@ -19,43 +19,58 @@ $addr = $_GET['addr'];
 $secret = $_GET['secret'];
 
 // Check all are set
-if(empty($txid) || empty($value) || empty($addr) || empty($secret)){
+if (empty($txid) || empty($value) || empty($addr) || empty($secret)) {
     exit();
 }
 
-if($secret != $secretlocal){
+if ($secret != $secretlocal) {
     exit();
 }
 
+if ($txid == 'WarningThisIsAGeneratedTestPaymentAndNotARealBitcoinTransaction') {
+    $txid = 'WarningThisIsATestTransaction - ' . $addr;
+} else {
+    $txid = $txid . " - " . $addr;
+}
 
-$sql = "INSERT INTO `payments` (`txid`, `value`, `addr`, `status`)
-VALUES ('$txid', '$value', '$addr', '$status')";
+$sql = "INSERT INTO `payments` (`txid`, `value`, `addr`, `status`) VALUES ('$txid', '$value', '$addr', '$status')";
 mysqli_query($conn, $sql);
-echo $sql;
+//echo $sql;
+
 // Get invoice price
 $code = getCode($addr);
 $price = getInvoicePrice($code);
+
 // Convert price to satoshi for check
 $price = USDtoBTC($price);
-$price = $price *100000000;
+$price = $price * 100000000;
 
-// Expired
-if($status < 0){
+// check all invoince payments and if total payment amount is greater than or equal to price, mark as paid
+// Get total amount paid
+$total_amount_paid_satoshi = getTotalInvoincePayments($addr);
+// Check if paid enough
+if ($total_amount_paid_satoshi >= round($price)) {
+    echo "Paid enough";
+    // Update invoice status
+    updateInvoiceStatus($code, 2);
     exit();
 }
 
+// Expired
+if ($status < 0) {
+    exit();
+}
 
-if($value >= round($price)){
+if ($value >= round($price)) {
     // Update invoice status
     updateInvoiceStatus($code, $status);
-    if($status == 2){
+    if ($status == 2) {
         // Correct amount paid and fully confirmed
         // Do whatever you want here once payment is correct
         $invoice = getInvoice($addr);
         createOrder($invoice, getInvoiceIp($addr));
     }
-}else {
+} else {
     // Buyer hasnt paid enough
     updateInvoiceStatus($code, -2);
 }
-?>
