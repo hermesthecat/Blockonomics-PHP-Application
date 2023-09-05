875e7374556da809dab1b2916ee86a795bd899b4
abdullah azad <4947845+hermesthecat@users.noreply.github.com>
2023-09-05 16:33:49 +0300

    Update orders.php

--- app/orders.php
+++ app/orders.php
@@ -1,17 +1,12 @@
 <?php
-/*
-Payment page
 
-This code is designed to be easily understandable at the expense of speed, 
-for large productions this can be done with one sql request, instead of several
-
-*/
 include_once "config.php";
 include_once "functions.php";
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -21,6 +16,7 @@ include_once "functions.php";
     <!-- Custom CSS -->
     <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
 
     <!-- Navigation bar -->
@@ -32,36 +28,36 @@ include_once "functions.php";
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="index.php">Store <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item active">
-                    <a class="nav-link" href="orders.php">Purchases</a>
-                </li>
-                
+                    <li class="nav-item active">
+                        <a class="nav-link" href="index.php">Store <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="invoices.php">Invoices</a>
+                    </li>
                 </ul>
-                
+
             </div>
         </div>
     </nav>
 
     <!-- Invoice -->
-    
+
     <main>
         <div class="row">
             <h1 style="width:100%;">Previous purchases</h1>
             <?php
             $ip = getIp();
-            $sql = "SELECT * FROM `orders` WHERE `ip`='$ip' ORDER BY `id` DESC";
+            //$sql = "SELECT * FROM `orders` WHERE `ip`='$ip' ORDER BY `id` DESC";
+            $sql = "SELECT * FROM `orders` ORDER BY `id` DESC";
             $result = mysqli_query($conn, $sql);
             // Check number of orders
-            if(mysqli_num_rows($result)==0){
+            if (mysqli_num_rows($result) == 0) {
                 // No previous orders
-                ?>
-                <p>No previous orders. This is figured out using your IP address</p>
-                <?php
-            }else {
-                ?>
+            ?>
+                <p>No previous orders.</p>
+            <?php
+            } else {
+            ?>
                 <table class="table table-striped">
                     <thead>
                         <tr>
@@ -71,28 +67,29 @@ include_once "functions.php";
                     </thead>
                     <tbody>
                         <?php
-                        while($row = mysqli_fetch_assoc($result)){
-                            ?>
+                        while ($row = mysqli_fetch_assoc($result)) {
+                        ?>
                             <tr>
                                 <th scope="row"><?php echo getProduct(getInvoiceProduct($row['invoice'])); ?></th>
                                 <td><a href="invoice.php?code=<?php echo $row['invoice']; ?>"><?php echo $row['invoice']; ?></a></td>
-                                
+
                             </tr>
-                            <?php
+                        <?php
                         }
                         ?>
                     </tbody>
                 </table>
-                <?php
+            <?php
             }
             ?>
         </div>
     </main>
-    
+
 
     <!-- Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 </body>
+
 </html>
