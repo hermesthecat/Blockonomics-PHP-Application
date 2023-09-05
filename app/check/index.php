1e3de66b24d82949f6a4b07183b8648b30010e24
abdullah azad <4947845+hermesthecat@users.noreply.github.com>
2023-09-05 17:27:33 +0300

    Update index.php

--- app/check/index.php
+++ app/check/index.php
@@ -1,16 +1,10 @@
 <?php
-/*
-Callback location, set this in blockonmics merchant page
 
-For testing payments locally, use this:
-localhost/bitcoin/check?secret=asecretcode&addr=[ADDRESS]&status=[STATUS CODE]&txid=[TXID]&value=[Amount paid in satoshi]
-*/
-include_once "../config.php";
-include_once "../functions.php";
+include_once "config.php";
+include_once "functions.php";
 
-$secretlocal = "asecretcode"; // Code in the callback, make sure this matches to what youve set
+$secretlocal = "tyututyuytuytuytut";
 
-// Get all these values
 $status = 0;
 $txid = $_GET['txid'];
 $value = $_GET['value'];
@@ -18,7 +12,6 @@ $status = $_GET['status'];
 $addr = $_GET['addr'];
 $secret = $_GET['secret'];
 
-// Check all are set
 if (empty($txid) || empty($value) || empty($addr) || empty($secret)) {
     exit();
 }
@@ -35,42 +28,29 @@ if ($txid == 'WarningThisIsAGeneratedTestPaymentAndNotARealBitcoinTransaction') 
 
 $sql = "INSERT INTO `payments` (`txid`, `value`, `addr`, `status`) VALUES ('$txid', '$value', '$addr', '$status')";
 mysqli_query($conn, $sql);
-//echo $sql;
 
-// Get invoice price
 $code = getCode($addr);
 $price = getInvoicePrice($code);
 
-// Convert price to satoshi for check
 $price = USDtoBTC($price);
 $price = $price * 100000000;
 
-// check all invoince payments and if total payment amount is greater than or equal to price, mark as paid
-// Get total amount paid
 $total_amount_paid_satoshi = getTotalInvoincePayments($addr);
-// Check if paid enough
 if ($total_amount_paid_satoshi >= round($price)) {
-    echo "Paid enough";
-    // Update invoice status
     updateInvoiceStatus($code, 2);
     exit();
 }
 
-// Expired
 if ($status < 0) {
     exit();
 }
 
 if ($value >= round($price)) {
-    // Update invoice status
     updateInvoiceStatus($code, $status);
     if ($status == 2) {
-        // Correct amount paid and fully confirmed
-        // Do whatever you want here once payment is correct
         $invoice = getInvoice($addr);
         createOrder($invoice, getInvoiceIp($addr));
     }
 } else {
-    // Buyer hasnt paid enough
     updateInvoiceStatus($code, -2);
 }
